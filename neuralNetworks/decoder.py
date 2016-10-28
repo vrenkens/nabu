b287a0081f2c4ff63a217fa98fc6f891bb7fb8ac
vrenkens <vincent.renkens@esat.kuleuven.be>
2016-10-28 14:25:56 +0200

    created a CTC decoder

--- neuralNetworks/decoder.py
+++ neuralNetworks/decoder.py
@@ -1,50 +1,80 @@
 '''@file decoder.py
 neural network decoder environment'''
 
+from abc import ABCMeta, abstractmethod
 import tensorflow as tf
 import numpy as np
 from classifiers import seq_convertors
 
-class Decoder(object):
-    '''Class for the decoding environment for a neural net classifier'''
 
-    def __init__(self, classifier, input_dim, max_length):
+class Decoder(object):
+    '''the abstract class for a decoder'''
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, classifier, input_dim, max_input_length):
         '''
-        NnetDecoder constructor, creates the decoding graph
+        Decoder constructor, creates the decoding graph
 
         Args:
             classifier: the classifier that will be used for decoding
             input_dim: the input dimension to the nnnetgraph
+            max_input_length: the maximum length of the inputs
         '''
 
         self.graph = tf.Graph()
-        self.max_length = max_length
+        self.max_input_length = max_input_length
 
         with self.graph.as_default():
 
             #create the inputs placeholder
             self.inputs = tf.placeholder(
-                tf.float32, shape=[max_length, input_dim], name='inputs')
+                tf.float32, shape=[max_input_length, input_dim], name='inputs')
 
             #create the sequence length placeholder
-            self.seq_length = tf.placeholder(
+            self.input_seq_length = tf.placeholder(
                 tf.int32, shape=[1], name='seq_length')
 
             split_inputs = tf.unpack(tf.expand_dims(self.inputs, 1))
 
             #create the decoding graph
-            logits, _, self.saver, _ = classifier(split_inputs, self.seq_length,
-                                               is_training=False, reuse=False,
-                                               scope='Classifier')
+            logits, _, self.saver, logits_seq_length = \
+                classifier(split_inputs, self.input_seq_length,
+                           is_training=False, reuse=False, scope='Classifier')
 
-            #convert logits to non sequence for the softmax computation
-            logits = seq_convertors.seq2nonseq(logits, self.seq_length)
-
-            #compute the outputs
-            self.outputs = tf.nn.softmax(logits)
+            #compute the outputs based on the classifier output logits
+            self.outputs = self.get_outputs(logits, logits_seq_length)
 
         #specify that the graph can no longer be modified after this point
         self.graph.finalize()
+
+    @abstractmethod
+    def get_outputs(self, logits, logits_seq_length):
+        '''
+        Compute the outputs based on the output classifier output logits
+
+        Args:
+            logits: a NxO tensor where N is the sequence length and O is the
+                classifier output dimension
+            logits_seq_length: the logits sequence length
+
+        Returns:
+            the outputs of the decoding graph
+        '''
+
+        raise NotImplementedError("Abstract method")
+
+    @abstractmethod
+    def process_decoded(self, decoded):
+        '''
+        do some postprocessing on the output of the decoding graph
+
+        Args:
+            decoded: the outputs of the decoding graph
+
+        Returns:
+            the processed outputs
+        '''
 
     def __call__(self, inputs):
         '''decode using the neural net
@@ -54,21 +84,23 @@ class Decoder(object):
                 number of frames and F is the input feature dimension
 
         Returns:
-            an NxO numpy array where N is the number of frames and O is the
-                neural net output dimension
+            the output of the decoder
         '''
 
         #get the sequence length
-        seq_length = [inputs.shape[0]]
+        inputs_seq_length = [inputs.shape[0]]
 
         #pad the inputs
         inputs = np.append(
-            inputs, np.zeros([self.max_length-inputs.shape[0], inputs.shape[1]])
-            , 0)
+            inputs, np.zeros([self.max_input_length-inputs.shape[0],
+                              inputs.shape[1]]), 0)
 
         #pylint: disable=E1101
-        return self.outputs.eval(feed_dict={self.inputs:inputs,
-                                            self.seq_length:seq_length})
+        decoded = self.outputs.eval(
+            feed_dict={self.inputs:inputs,
+                       self.inputs_seq_length:inputs_seq_length})
+
+        return self.process_decoded(decoded)
 
     def restore(self, filename):
         '''
@@ -79,3 +111,106 @@ class Decoder(object):
         '''
 
         self.saver.restore(tf.get_default_session(), filename)
+
+class SimpleDecoder(Decoder):
+    '''Simple decoder that passes the output logits through a softmax'''
+
+    def get_outputs(self, logits, logits_seq_length):
+        '''
+        Put the classifier output logits through a softmax
+
+        Args:
+            logits: A list containing a 1xO tensor for each timestep where O
+                is the classifier output dimension
+            logits_seq_length: the logits sequence length
+        Returns:
+            An NxO tensor containing posterior distributions
+        '''
+
+        #convert logits to non sequence for the softmax computation
+        logits = seq_convertors.seq2nonseq(logits, logits_seq_length)
+
+        return tf.nn.softmax(logits)
+
+    def process_decoded(self, decoded):
+        '''
+        do nothing
+
+        Args:
+            decoded: the outputs of the decoding graph
+
+        Returns:
+            the outputs of the decoding graph
+        '''
+
+        return decoded
+
+class CTCDecoder(Decoder):
+    '''CTC Decoder'''
+
+    def __init__(self, classifier, input_dim, max_input_length, beam_width):
+        '''
+        Decoder constructor, creates the decoding graph
+
+        Args:
+            classifier: the classifier that will be used for decoding
+            input_dim: the input dimension to the nnnetgraph
+            max_input_length: the maximum length of the inputs
+            beam_width: the width of the decoding beam
+        '''
+
+        #store the beam width
+        self.beam_width = beam_width
+
+        super(CTCDecoder, self).__init__(classifier, input_dim,
+                                         max_input_length)
+
+    def get_outputs(self, logits, logits_seq_length):
+        '''
+        get the outputs with ctc beam search
+
+        Args:
+            logits: A list containing a 1xO tensor for each timestep where O
+                is the classifier output dimension
+            logits_seq_length: the logits sequence length
+
+        Returns:
+            a pair containg:
+                - a tuple of length W containing vectors with output sequences
+                - a W dimensional vector containing the log probabilities
+        '''
+
+        #do the CTC beam search
+        sparse_outputs, logprobs = tf.nn.ctc_beam_search_decoder(
+            tf.pack(logits), logits_seq_length, self.beam_width,
+            self.beam_width)
+
+        #convert the outputs to dense tensors
+        dense_outputs = [
+            tf.reshape(tf.sparse_tensor_to_dense(o, default_value=-1), [-1])
+            for o in sparse_outputs]
+
+        return tf.tuple([tf.tuple(dense_outputs), tf.reshape(logprobs,[-1])])
+
+    def process_decoded(self, decoded):
+        '''
+        create numpy arrays of decoded targets
+
+        Args:
+            decoded: a pair containing:
+                - a tuple of length W containing vectors with output sequences
+                - a W dimensional vector containing the log probabilities
+
+        Returns:
+            decoded: a pair containing:
+                - a list of output sequences with the padding removed
+                - a W dimensional vector containing the log probabilities
+        '''
+
+        target_sequences = decoded[0]
+        logprobs = decoded[1]
+
+        #remove the padding of -1 values from the target sequences
+        target_sequences = [s[np.where(s != -1)] for s in target_sequences]
+
+        return target_sequences, logprobs
