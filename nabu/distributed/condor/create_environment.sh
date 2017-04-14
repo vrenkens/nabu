53669f9a1c480b1bf2839744fd30e2a75df47a42
vrenkens <vincent.renkens@esat.kuleuven.be>
2017-04-14 14:21:01 +0200

    updated to tf 1.1

--- nabu/distributed/condor/create_environment.sh
+++ nabu/distributed/condor/create_environment.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
 #create the necesary environment variables
-source ~spch/soft/sgu21/srclogin/.bashrc
-
-if [ -d "/usr/local/cuda-8.0/lib64" ]
-then
-  echo "using local cuda"
-  export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/:$LD_LIBRARY_PATH
-else
-  echo "using nfs cuda"
-  export LD_LIBRARY_PATH=/users/spraak/vrenkens/cuda-8.0/lib64:$LD_LIBRARY_PATH
-fi
+source ~/.bashrc
 
 #copy the ssh binary to enable ssh tunnels
 cp /usr/bin/ssh /tmp
