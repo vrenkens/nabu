546c95c992fa9d8199a06477d471158799af3659
vrenkens <vincent.renkens@esat.kuleuven.be>
2017-11-13 17:17:12 +0100

    added code to automatically test all recipes

---
+++ nabu/scripts/test_recipe.py
@@ -0,0 +1,45 @@
+'''@file test_recipe.py
+test if a recipe can build the graphs correctly'''
+
+import sys
+import os
+import traceback
+import train
+import test
+import decode
+
+def main(recipe):
+    '''main function
+
+    args:
+        recipe: the recipe to be tested
+    '''
+
+    sys.stderr = open(os.devnull, 'w')
+
+    #test training
+    try:
+        sys.stdout = open(os.devnull, 'w')
+        train.train(
+            clusterfile=None,
+            job_name='local',
+            task_index=0,
+            ssh_command='None',
+            expdir=recipe,
+            testing=True
+        )
+        decode.decode(recipe, True)
+        test.test(recipe, True)
+        sys.stdout = sys.__stdout__
+
+    #pylint: disable=W0702
+    except:
+        sys.stdout = sys.__stdout__
+        print 'error for recipe %s:' % recipe
+        print traceback.format_exc()
+        return
+
+    print '%s OK' % recipe
+
+if __name__ == '__main__':
+    main(sys.argv[1])
